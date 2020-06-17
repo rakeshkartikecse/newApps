de384a18836fc229da99101f440d4a85b510b2b1
NewApps <rakeshkartikecse@gmail.com>
2020-06-17 15:07:29 +0530

    Adding variable name to Application.js

--- src/Application.js
+++ src/Application.js
@@ -3,9 +3,10 @@ import React, {Component} from 'react';
 class Application extends Component{
 
     render(){
+        let name="Kartike";
         return(
             <div>
-                <h1>Hello World!</h1>
+                <h1>Hello World! {name}</h1>
             <span>This is the first page of the React Application.</span>
             </div>
             
